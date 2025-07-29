9a7fbf1b109cc11a7c3aac2d9f8222495abad1be
Brian Chang <brimatt062495@gmail.com>
2025-07-29 11:14:47 -0400

    update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:5295/api' // change URL to deployed URL, like http://deployed-url/api
+  apiBaseUrl: 'https://drhemo-api-e6fbane7ejdcg8e9.centralus-01.azurewebsites.net/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:5295/api' // change URL to deployed URL, like http://deployed-url/api
+  apiBaseUrl: 'https://drhemo-api-e6fbane7ejdcg8e9.centralus-01.azurewebsites.net/api'
+ // change URL to deployed URL, like http://deployed-url/api
+  // apiBaseUrl: 'https://cresmedev.med.ucf.edu/drhemo/api'
 };
